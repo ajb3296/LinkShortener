7ca81f2457b0028f93df0e3b50cca8860c62eecc
An Jaebeom <ajb8533296@gmail.com>
2023-11-02 15:43:07 +0900

    add https

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
-use actix_web::{web, App, HttpServer};
+use std::path::Path;
 use actix_files as fs;
+use actix_web::{web, App, HttpServer};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 mod modules;
 mod redirect;
@@ -45,16 +47,41 @@ async fn main() -> std::io::Result<()> {
 
     let shared_data = web::Data::new(pool);
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(shared_data.clone())
-            .service(fs::Files::new("/static", "./static").show_files_listing())
-            .service(index::index)
-            .service(redirect::redirect)
-            .service(enroll::enroll)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .workers(4)
-    .run()
-    .await
+    let file_path = "key.pem";
+    
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    
+    let path = Path::new(file_path);
+    if path.exists() {
+        println!("Starting server with SSL");
+        builder.set_private_key_file("key.pem", SslFiletype::PEM).unwrap();
+        builder.set_certificate_chain_file("cert.pem").unwrap();
+        
+        HttpServer::new(move || {
+            App::new()
+                .app_data(shared_data.clone())
+                .service(fs::Files::new("/static", "./static").show_files_listing())
+                .service(index::index)
+                .service(redirect::redirect)
+                .service(enroll::enroll)
+        })
+        .bind_openssl("0.0.0.0:8080", builder)?
+        .workers(4)
+        .run()
+        .await
+    } else {
+        println!("Starting server without SSL");
+        HttpServer::new(move || {
+            App::new()
+                .app_data(shared_data.clone())
+                .service(fs::Files::new("/static", "./static").show_files_listing())
+                .service(index::index)
+                .service(redirect::redirect)
+                .service(enroll::enroll)
+        })
+        .bind(("0.0.0.0", 8080))?
+        .workers(4)
+        .run()
+        .await
+    }
 }
